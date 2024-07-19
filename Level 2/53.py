99fd1422534dbc59316f4e8cf1c2c3cd83875d14
Sunwoo Kim (MacBook) <sunshower1127@gmail.com>
2024-07-19 13:10:52 +0900

    53번 풂

---
+++ Level 2/53.py
@@ -0,0 +1,27 @@
+"""
+
+가장 큰 수
+
+6, 10,2 라면 6210 으로 가장 큰 수 만들기.
+
+처음엔 빈자리에 A를 채워넣어서 정렬하면 될거라고 생각했는데, 아니였음.
+숫자가 나열되는건 반복되는것이기때문에 *3을 하는게 맞음.
+
+tip : 다른 배열을 기준으로 정렬할때는 zip하고 정렬한 후 zip(* )을 쓰면 됨.
+tip : 머릿속에서 복잡한 함수 쥐어짜내는것보다
+    그냥 for if 도배로 구현하는게 더 빠를 수 있음.
+
+"""
+
+
+def SortBy(a, b):
+    Sorted = sorted(zip(a, b), reverse=True)
+    return zip(*Sorted)[0]
+
+
+def solution(Nums):
+    Strs = [str(Num) * 3 for Num in Nums]
+
+    Nums = SortBy(Nums, Strs)
+
+    return "".join(str(Num) for Num in Nums)
