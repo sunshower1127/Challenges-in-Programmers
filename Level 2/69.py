a4aa3e1cc10793f91b1c176f1b7cea1542b3ea71
Sunwoo Kim (MacBook) <sunshower1127@gmail.com>
2024-09-05 13:02:26 +0900

    69번 1차 풀기

---
+++ Level 2/69.py
@@ -0,0 +1,44 @@
+"""
+
+[3차] 방금 그곡
+
+시작시간, 종료시간, 음악제목, 악보정보.
+반복해서 이어 붙여야함 시간만큼.
+그중에서 해당 문자열을 찾으면 반환.
+재생시간, 음악제목 순으로 정렬
+
+일단 실패함. 부분케이스만 해결하면 될듯.
+"""
+
+
+def convert(notes):
+    new_notes = []
+    for note in notes:
+        if note == "#":
+            new_notes[-1] = new_notes[-1].lower()
+        else:
+            new_notes.append(note)
+
+    return "".join(new_notes)
+
+
+def get_mins(t):
+    h, m = map(int, t.split(":"))
+    return h * 60 + m
+
+
+def solution(m, musicinfos):
+    m = convert(m)
+    ls = []
+    for info in musicinfos:
+        begin_t, end_t, title, notes = info.split(",")
+        t = get_mins(end_t) - get_mins(begin_t)
+        ls.append((t, title, convert(notes)))
+
+    ls.sort(reverse=True, key=lambda x: x[0])
+
+    for t, title, notes in ls:
+        q, r = divmod(t, len(notes))
+        notes = notes * q + notes[:r]
+        if m in notes:
+            return title
